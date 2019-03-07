604663bacec8ed9c09db42f7bf684be55ca0c52e
Svetlin Ralchev <svet@ralch.com>
2019-03-07 08:52:31 +0100

    Add example file

--- hydra/example.tf
+++ hydra/example.tf
@@ -1,12 +1,3 @@
-# Ory Hydra
-
-It's a terraform provider that allow creating a client in [Ory Hydra OAuth2](https://www.ory.sh) server.
-
-## Usage
-
-You can use the [example](./example.tf) or code snippet below:
-
-```
 provider "hydra" {
   endpoint = "http://localhost:4445"
 }
@@ -14,9 +5,8 @@ provider "hydra" {
 resource "hydra_client" "guest" {
   description    = "A guest credentials"
   client_id      = "guest"
-  client_secret  = "guest1234"
+  client_secret  = "swordfish"
   scope          = "hydra.warden hydra.keys.* hydra.introspect"
   grant_types    = ["client_credentials", "refresh_token"]
   response_types = ["token"]
 }
-```
