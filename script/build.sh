b8a0338896321fe3acb038117fe966a7cbec8fbb
Svetlin Ralchev <svet@ralch.com>
2019-03-19 15:37:00 +0100

    Add version number in the binary name

--- script/build.sh
+++ script/build.sh
@@ -4,13 +4,16 @@ mkdir -p "$PWD/build"
 
 export PROVIDER="terraform-provider-$1"
 export CGO_ENABLED=0
+export VERSION
 
 build_for_arch() {
  export GOOS="$1"
  export GOARCH="$2"
 
- go build -o "$PWD/build/$PROVIDER-$GOOS-$GOARCH" "github.com/phogolabs/terraform-provider/cmd/$PROVIDER"
+ go build -o "$PWD/build/${PROVIDER}_${VERSION}-${GOOS}-${GOARCH}" "github.com/phogolabs/terraform-provider/cmd/${PROVIDER}"
 }
+
+VERSION="$(git describe --tags --abbrev=0)"
 
 build_for_arch "linux" "amd64"
 build_for_arch "linux" "386"
